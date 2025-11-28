184dc4addf0854c3916974d96fbdc9cba9e3aa4e
Parth P <parth.p@crestinfosystems.net>
2025-11-28 12:19:35 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/crest-volleyball-tournament/', 
   plugins: [tailwindcss(), react()],
 })
